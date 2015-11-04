cc80b91bd3df7dbffa054919e88951fc9ca2b5c4
alihopp <alina.hoppe@hhu.de>
2015-11-04 15:20:56 +0100

    Homework_1

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,24 @@
+#include<iostream>
+using namespace std;
 
+int fib (int N);
+int main(){
+	int N;
+	
+	cout << "Zahl eingeben:";
+	cin >> N;
+	cout << "Fibonacci ="<< fib(N)<< endl;
+}
+
+int fib (int N){
+if (N==0){
+	return 0;
+}
+
+if (N==1){
+	return 1;
+}
+
+return fib(N-1) + fib(N-2);
+
+}
